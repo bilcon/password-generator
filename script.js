85fb952349ad23ffaad258542a1f0fa394804d9a
Bill Morris <bilcon@swbell.net>
2021-10-10 06:21:06 -0500

    JavaScript begun with character sets

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// User input variables: 
+var enter;
+var confirmLowercase;
+var confirmUppercase;
+var confirmNumber;
+var confirmCharacter;
+
+// Starting Password variable values: 
+// Lowercase characters
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// Uppercase characters
+alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+// Numeric characters
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// Special characters 
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "<", "=", ">", "?", "@", "[", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
