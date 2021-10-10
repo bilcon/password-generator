662f60cb454296e522d3170eb18772420ecf94d4
Bill Morris <bilcon@swbell.net>
2021-10-10 09:30:50 -0500

    prompts for what characters to use

--- script.js
+++ script.js
@@ -15,3 +15,32 @@ alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "
 number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // Special characters 
 character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "<", "=", ">", "?", "@", "[", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+
+var get = document.querySelector("#generate");
+
+get.addEventListener("click", function () {
+    ps = generatePassword();
+    document.getElementById("password").placeholder = ps;
+});
+
+// Starts generating password function
+function generatePassword() {
+    // Asks for input how many characters
+    enter = parseInt(prompt("How many characters do you want in your password?\n\nEnter a number between 8 and 128\n"));
+    // First if statement for user validation 
+    if (!enter) {
+        alert("You must enter an actual number between 8 and 128");
+    } else if (enter < 8 || enter > 128) {
+    // Validates user input
+
+        enter = parseInt(prompt("You must enter an actual number between 8 and 128"));
+
+    } else {
+    // Continues once user input is validated
+    // Start user input prompts
+
+        confirmLowercase = confirm("Do you want to use Lowercase letters?\n\nSelect OK for YES or Cancel for NO");
+        confirmUppercase = confirm("Do you want to use Uppercase letters?\n\nSelect OK for YES or Cancel for NO");
+        confirmNumber = confirm("Do you want to use a number?\n\nSelect OK for YES or Cancel for NO");
+        confirmCharacter = confirm("Do you want to use special characters?\n\nSelect OK for YES or Cancel for NO");
+    };
